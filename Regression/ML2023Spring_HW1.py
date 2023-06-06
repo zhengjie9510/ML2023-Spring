0ed474b83cb2934cdb4bc0fff8dbf47b5b0ed0d7
zhengjie9510 <zhengjie9510@qq.com>
2023-06-06 15:04:27 +0800

    update

--- Regression/ML2023Spring_HW1.py
+++ Regression/ML2023Spring_HW1.py
@@ -6,7 +6,7 @@
 # * Solve a regression problem with deep neural networks (DNN).
 # * Understand basic DNN training tips.
 # * Familiarize yourself with PyTorch.
-# 
+#
 # If you have any questions, please contact the TAs via TA hours, NTU COOL, or email to mlta-2023-spring@googlegroups.com
 
 # %% [markdown]
@@ -16,7 +16,6 @@
 # Numerical Operations
 import math
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 
 # Reading/Writing Data
 import pandas as pd
@@ -28,7 +27,7 @@ import time
 from tqdm import tqdm
 
 # Pytorch
-import torch 
+import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
 
@@ -37,11 +36,12 @@ from torch.utils.tensorboard import SummaryWriter
 
 # %% [markdown]
 # # Some Utility Functions
-# 
+#
 # You do not need to modify this part.
-
 # %%
-def same_seed(seed): 
+
+
+def same_seed(seed):
     '''Fixes random number generator seeds for reproducibility.'''
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
@@ -50,33 +50,39 @@ def same_seed(seed):
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
 
+
 def train_valid_split(data_set, valid_ratio, seed):
     '''Split provided training data into training set and validation set'''
-    valid_set_size = int(valid_ratio * len(data_set)) 
+    valid_set_size = int(valid_ratio * len(data_set))
     train_set_size = len(data_set) - valid_set_size
-    train_set, valid_set = random_split(data_set, [train_set_size, valid_set_size], generator=torch.Generator().manual_seed(seed))
+    train_set, valid_set = random_split(data_set, [
+                                        train_set_size, valid_set_size], generator=torch.Generator().manual_seed(seed))
     return np.array(train_set), np.array(valid_set)
 
+
 def predict(test_loader, model, device):
-    model.eval() # Set your model to evaluation mode.
+    model.eval()  # Set your model to evaluation mode.
     preds = []
     for x in tqdm(test_loader):
-        x = x.to(device)                        
-        with torch.no_grad():                   
-            pred = model(x)                     
-            preds.append(pred.detach().cpu())   
-    preds = torch.cat(preds, dim=0).numpy()  
+        x = x.to(device)
+        with torch.no_grad():
+            pred = model(x)
+            preds.append(pred.detach().cpu())
+    preds = torch.cat(preds, dim=0).numpy()
     return preds
 
 # %% [markdown]
 # # Dataset
 
 # %%
+
+
 class COVID19Dataset(Dataset):
     '''
     x: Features.
     y: Targets, if none, do prediction.
     '''
+
     def __init__(self, x, y=None):
         if y is None:
             self.y = y
@@ -98,10 +104,12 @@ class COVID19Dataset(Dataset):
 # Try out different model architectures by modifying the class below.
 
 # %%
+
+
 class My_Model(nn.Module):
     def __init__(self, input_dim):
         super(My_Model, self).__init__()
-        # TODO: modify model's structure, be aware of dimensions. 
+        # TODO: modify model's structure, be aware of dimensions.
         self.layers = nn.Sequential(
             nn.Linear(input_dim, 32),
             nn.LeakyReLU(),
@@ -114,7 +122,7 @@ class My_Model(nn.Module):
 
     def forward(self, x):
         x = self.layers(x)
-        x = x.squeeze(1) # (B, 1) -> (B)
+        x = x.squeeze(1)  # (B, 1) -> (B)
         return x
 
 # %% [markdown]
@@ -122,39 +130,49 @@ class My_Model(nn.Module):
 # Choose features you deem useful by modifying the function below.
 
 # %%
+
+
 def select_feat(train_data, valid_data, test_data, select_all=True):
     '''Selects useful features to perform regression'''
-    y_train, y_valid = train_data[:,-1], valid_data[:,-1]
-    raw_x_train, raw_x_valid, raw_x_test = train_data[:,:-1], valid_data[:,:-1], test_data
+    y_train, y_valid = train_data[:, -1], valid_data[:, -1]
+    raw_x_train, raw_x_valid, raw_x_test = train_data[:,
+                                                      :-1], valid_data[:, :-1], test_data
 
     if select_all:
         feat_idx = list(range(raw_x_train.shape[1]))
     else:
-        feat_idx = [34, 35, 36, 43, 46, 47, 51, 52, 53, 54, 61, 64, 65, 69, 70, 71, 72, 79, 82, 83] # TODO: Select suitable feature columns.
-        
-    return raw_x_train[:,feat_idx], raw_x_valid[:,feat_idx], raw_x_test[:,feat_idx], y_train, y_valid
+        # TODO: Select suitable feature columns.
+        feat_idx = [34, 35, 36, 43, 46, 47, 51, 52, 53,
+                    54, 61, 64, 65, 69, 70, 71, 72, 79, 82, 83]
+
+    return raw_x_train[:, feat_idx], raw_x_valid[:, feat_idx], raw_x_test[:, feat_idx], y_train, y_valid
 
 # %% [markdown]
 # # Training Loop
 
 # %%
+
+
 def trainer(train_loader, valid_loader, model, config, device):
 
-    criterion = nn.MSELoss(reduction='mean') # Define your loss function, do not modify this.
+    # Define your loss function, do not modify this.
+    criterion = nn.MSELoss(reduction='mean')
 
-    # Define your optimization algorithm. 
+    # Define your optimization algorithm.
     # TODO: Please check https://pytorch.org/docs/stable/optim.html to get more available algorithms.
     # TODO: L2 regularization (optimizer(weight decay...) or implement by your self).
-    optimizer = torch.optim.SGD(model.parameters(), lr=config['learning_rate'], momentum=0.7) 
-    writer = SummaryWriter(f"runs/{time.strftime('%Y-%m-%d_%H-%M-%S')}") # Writer of tensoboard.
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=config['learning_rate'], momentum=0.7)
+    # Writer of tensoboard.
+    writer = SummaryWriter(f"runs/{time.strftime('%Y-%m-%d_%H-%M-%S')}")
 
     if not os.path.isdir('./models'):
-        os.mkdir('./models') # Create directory of saving models.
+        os.mkdir('./models')  # Create directory of saving models.
 
     n_epochs, best_loss, step, early_stop_count = config['n_epochs'], math.inf, 0, 0
 
     for epoch in range(n_epochs):
-        model.train() # Set your model to train mode.
+        model.train()  # Set your model to train mode.
         loss_record = []
 
         # tqdm is a package to visualize your training progress.
@@ -162,23 +180,23 @@ def trainer(train_loader, valid_loader, model, config, device):
 
         for x, y in train_pbar:
             optimizer.zero_grad()               # Set gradient to zero.
-            x, y = x.to(device), y.to(device)   # Move your data to device. 
-            pred = model(x)             
+            x, y = x.to(device), y.to(device)   # Move your data to device.
+            pred = model(x)
             loss = criterion(pred, y)
-            loss.backward()                     # Compute gradient(backpropagation).
+            # Compute gradient(backpropagation).
+            loss.backward()
             optimizer.step()                    # Update parameters.
             step += 1
             loss_record.append(loss.detach().item())
-            
+
             # Display current epoch number and loss on tqdm progress bar.
             train_pbar.set_description(f'Epoch [{epoch+1}/{n_epochs}]')
             train_pbar.set_postfix({'loss': loss.detach().item()})
 
-
         mean_train_loss = sum(loss_record)/len(loss_record)
         # writer.add_scalar('Loss/train', mean_train_loss, step)
 
-        model.eval() # Set your model to evaluation mode.
+        model.eval()  # Set your model to evaluation mode.
         loss_record = []
         for x, y in valid_loader:
             x, y = x.to(device), y.to(device)
@@ -187,19 +205,22 @@ def trainer(train_loader, valid_loader, model, config, device):
                 loss = criterion(pred, y)
 
             loss_record.append(loss.item())
-            
+
         mean_valid_loss = sum(loss_record)/len(loss_record)
-        print(f'Epoch [{epoch+1}/{n_epochs}]: Train loss: {mean_train_loss:.4f}, Valid loss: {mean_valid_loss:.4f}')
+        print(
+            f'Epoch [{epoch+1}/{n_epochs}]: Train loss: {mean_train_loss:.4f}, Valid loss: {mean_valid_loss:.4f}')
         # writer.add_scalar('Loss/valid', mean_valid_loss, step)
 
-        writer.add_scalars('loss',{'train':mean_train_loss,'valid':mean_valid_loss},step)
+        writer.add_scalars(
+            'loss', {'train': mean_train_loss, 'valid': mean_valid_loss}, step)
 
         if mean_valid_loss < best_loss:
             best_loss = mean_valid_loss
-            torch.save(model.state_dict(), config['save_path']) # Save your best model
+            # Save your best model
+            torch.save(model.state_dict(), config['save_path'])
             print('Saving model with loss {:.3f}...'.format(best_loss))
             early_stop_count = 0
-        else: 
+        else:
             early_stop_count += 1
 
         if early_stop_count >= config['early_stop']:
@@ -212,16 +233,18 @@ def trainer(train_loader, valid_loader, model, config, device):
 # # Configurations
 # `config` contains hyper-parameters for training and the path to save your model.
 
+
 # %%
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 config = {
     'seed': 5201314,      # Your seed number, you can pick your lucky number. :)
     'select_all': False,   # Whether to use all features.
     'valid_ratio': 0.2,   # validation_size = train_size * valid_ratio
-    'n_epochs': 10000,     # Number of epochs.            
-    'batch_size': 256, 
-    'learning_rate': 1e-4,              
-    'early_stop': 600,    # If model has not improved for this many consecutive epochs, stop training.     
+    'n_epochs': 10000,     # Number of epochs.
+    'batch_size': 256,
+    'learning_rate': 1e-4,
+    # If model has not improved for this many consecutive epochs, stop training.
+    'early_stop': 600,
     'save_path': './models/model.ckpt'  # Your model will be saved here.
 }
 
@@ -232,8 +255,10 @@ config = {
 
 # %%
 same_seed(config['seed'])
-train_data, test_data = pd.read_csv('./covid_train.csv').values, pd.read_csv('./covid_test.csv').values
-train_data, valid_data = train_valid_split(train_data, config['valid_ratio'], config['seed'])
+train_data, test_data = pd.read_csv(
+    './covid_train.csv').values, pd.read_csv('./covid_test.csv').values
+train_data, valid_data = train_valid_split(
+    train_data, config['valid_ratio'], config['seed'])
 
 # Print out the data size.
 print(f"""train_data size: {train_data.shape} 
@@ -241,7 +266,8 @@ valid_data size: {valid_data.shape}
 test_data size: {test_data.shape}""")
 
 # Select features
-x_train, x_valid, x_test, y_train, y_valid = select_feat(train_data, valid_data, test_data, config['select_all'])
+x_train, x_valid, x_test, y_train, y_valid = select_feat(
+    train_data, valid_data, test_data, config['select_all'])
 # 对训练集进行归一化并保存归一化参数
 # scaler = MinMaxScaler()
 # x_train= scaler.fit_transform(x_train)
@@ -252,19 +278,23 @@ x_train, x_valid, x_test, y_train, y_valid = select_feat(train_data, valid_data,
 print(f'number of features: {x_train.shape[1]}')
 
 train_dataset, valid_dataset, test_dataset = COVID19Dataset(x_train, y_train), \
-                                            COVID19Dataset(x_valid, y_valid), \
-                                            COVID19Dataset(x_test)
+    COVID19Dataset(x_valid, y_valid), \
+    COVID19Dataset(x_test)
 
 # Pytorch data loader loads pytorch dataset into batches.
-train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True, pin_memory=True)
-valid_loader = DataLoader(valid_dataset, batch_size=config['batch_size'], shuffle=True, pin_memory=True)
-test_loader = DataLoader(test_dataset, batch_size=config['batch_size'], shuffle=False, pin_memory=True)
+train_loader = DataLoader(
+    train_dataset, batch_size=config['batch_size'], shuffle=True, pin_memory=True)
+valid_loader = DataLoader(
+    valid_dataset, batch_size=config['batch_size'], shuffle=True, pin_memory=True)
+test_loader = DataLoader(
+    test_dataset, batch_size=config['batch_size'], shuffle=False, pin_memory=True)
 
 # %% [markdown]
 # # Start training!
 
 # %%
-model = My_Model(input_dim=x_train.shape[1]).to(device) # put your model and data on the same computation device.
+# put your model and data on the same computation device.
+model = My_Model(input_dim=x_train.shape[1]).to(device)
 trainer(train_loader, valid_loader, model, config, device)
 
 
@@ -281,7 +311,8 @@ def save_pred(preds, file):
         for i, p in enumerate(preds):
             writer.writerow([i, p])
 
+
 model = My_Model(input_dim=x_train.shape[1]).to(device)
 model.load_state_dict(torch.load(config['save_path']))
-preds = predict(test_loader, model, device) 
+preds = predict(test_loader, model, device)
 save_pred(preds, 'pred.csv')
